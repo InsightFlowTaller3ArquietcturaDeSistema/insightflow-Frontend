489c4c05a89f9ede613590ec3e20414b7566b720
FranciscoConcha <Francisco.concha.urquieta@gmail.com>
2025-12-07 03:20:56 -0300

    ci: feat(cors): Se cambio la url del backen user-services

--- src/pages/loginPage.tsx
+++ src/pages/loginPage.tsx
@@ -15,7 +15,7 @@ export default function Login(){
     const handleLogin = async (e: React.FormEvent<HTMLFormElement>) =>{
         e.preventDefault();
         try{
-            const res = await axios.post("http://localhost:3000/usuario/login",{
+            const res = await axios.post("https://user-services-13hx.onrender.com/usuario/login",{
                 usuario,
                 contrasenia
             });
